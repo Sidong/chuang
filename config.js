ab797d35738df17df92cdd3f17bb46f2e3da6f07
sidongh <sidongh@gmail.com>
2015-09-02 12:04:09 +0800

    fix dialog.css

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-	name: 'DXXD',
+	name: '创次方',
 	port: 3008,
 	AvosAppID: 'fzj3thmtc2ddlxz7xx5qh8tcqdg0u5p5vrsevvfqs8fux0vu',
 	AvosAppkey: '1t1bjqbvch95mfjbc3lxiou69eaxiqw3j2f8nmrazq2qtrqm'
